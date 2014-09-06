02f672ceb26c6bd3487971557f61af0b3a3612da
thatdevgirl <joni@jhalabi.com>
2014-09-05 20:40:02 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+Backgrid Cell Extensions
+=========================
+
+__Author:__ Joni Halabi (www.jhalabi.com)
+
+Description
+-----------
+
+This library contains several Backgrid cell extensions that I have found useful for various projects.  You can learn more about Backgrid at http://backgridjs.com/.
+
+StatusFormatterCell
+-----------
+
+This cell extends the Backgrid Cell so that it formats status cells appropriately.  Formatting is based on the model value of the given cell and relies on adding a class name to a given cell.  Example CSS is given.
+
+Cell values and classes can be added or modified as necessary.  The cases given here are examples.
+
+ImageCell
+-----------
+
+This cell extends the Backgrid Cell so that it inserts an image into the table cell.  Model data should contain the URI of the image.
+
+BooleanCell
+-----------
+
+This cell extends the Backgrid Cell so that it replaces a boolean model property value with either "yes" or "no".  Model data should (obviously) be a boolean value.
---
+++ backgrid-cell-extensions.js
@@ -0,0 +1,90 @@
+/*
+ * TITLE: StatusFormatterCell
+ *
+ * Extend the Backgrid Cell so that it formats status cells appropriately.
+ * Formatting is based on the model value of the given cell.
+ */
+
+var StatusFormatterCell = Backgrid.Cell.extend({
+	render: function() {
+		this.$el.empty();
+		
+		var _status = this.model.get(this.column.attributes.name);
+		var _class = "status ";
+		
+		// Set an additional class name to the cell based on the status.
+		switch(_status.toUpperCase()) {
+			case "SENT":
+			case "ON":
+			case "LIVE":
+				_class = "good";
+				break;
+				
+			case "HALTED":
+			case "CANCELLED":
+				_class = "bad";
+				break;
+				
+			case "UNSCHEDULED":
+			case "OFF":
+				_class = "really-bad";
+				break;
+				
+			case "DISABLED":
+			case "DRAFT":
+				_class = "disabled";
+				break;
+				
+			case "NOTGENERATED":
+				_class = "medium";
+				break;
+			default:
+				break;
+		}
+		
+		var _html = '<p class="' + _class + '">' + _status + '</p>';
+		this.$el.html(_html);
+		
+		return this;
+	}
+});
+
+
+/*
+ * TITLE: ImageCell
+ *
+ * Extend the Backgrid Cell so that it inserts an image into the table cell.
+ * Model data should contain the URI of the image.
+ */
+
+var ImageCell = Backgrid.Cell.extend({
+	render: function() {
+		this.$el.empty();
+		
+		var _value = this.model.get(this.column.attributes.name);
+		var _html = '<img src="' + _value + '" />';
+		
+		this.$el.html(_html);
+		return this;
+	}
+});
+
+
+/*
+ * TITLE: BooleanCell
+ *
+ * Extend the Backgrid Cell so that it replaces a boolean model property value with either "yes" or "no".
+ * Model data should (obviously) be a boolean value.
+ */
+
+var BooleanCell = Backgrid.Cell.extend({
+	render: function() {
+		this.$el.empty();
+		
+		var _value = this.model.get(this.column.attributes.name);
+		var _html = (_value === true) ? 'Yes' : 'No';
+		
+		this.$el.html(_html);
+		return this;
+	}
+});
